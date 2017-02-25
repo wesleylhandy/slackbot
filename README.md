54b7b715f6a544195aae3465d94ccb33971c5b4d
Wesley Handy <wesleylhandy@gmail.com>
2017-02-25 01:16:04 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+1. Fork this repo, and clone your forked repo.
+
+2. Run `npm install`
+
+	* Within `app.js` choose your own api call and customize this code for your own slack commands.
+
+3. Push this repo onto a server (example: heroku)
+
+4. Copy YOUR server url: (example: http://foo-barrr-15071.herokuapp.com/)
+
+5. Go to: https://api.slack.com/custom-integrations
+
+6. Click: New /command
+
+7. Click: Add Configuration
+
+8. choose a command: /foobar 
+
+9. choose method: GET
+
+10. paste URL from step 3.
+
+11. save integration
---
+++ app.js
@@ -0,0 +1,56 @@
+var Slack = require('slack-node');
+var express = require('express');
+var url = require('url');
+var app = express();
+var request = require('request'); 
+
+
+////////////// THE SETUP ///////////////////////////////////////////
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'))
+
+app.get('/', function(request, response) {
+
+    var urlObject = url.parse(request.url,true).query
+    console.log(urlObject)
+    newIdentity(urlObject);
+
+}); //app.get
+
+
+/////////////// THE SEND MESSAGE //////////////////////////////////////////
+
+function newIdentity (urlObject){
+
+    slack = new Slack();
+    slack.setWebhook(urlObject.response_url);
+
+
+    //   /mySlashCommand catfish    'catfish' is stored in var userCommand
+    var userCommand = urlObject.text.toLowerCase();
+
+    if(userCommand !== "male" || userCommand !== "female") {
+        //respond with error message
+    }
+
+    // API call
+    request('http://uinames.com/api/?gender=' + userCommand, function (error, response, body) {
+
+        var data = JSON.parse(body);
+
+        slack.webhook({
+         channel: urlObject.channel_name,
+          text: "Your new identity is " + data.name + " " + data.surname +". And your new life awaits you in " + data.region + "." //  the response back to slack
+        }, function(err, response) {
+            if (err){
+                console.log(err)
+            }
+        })//webhook
+
+    })//request
+}
+
+/////////////////////////////////////////////////////////
+
